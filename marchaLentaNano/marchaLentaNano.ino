b94652cb7277c743a2c97b201c00ddb761c994da
aluferox <aluferoxx@hotmail.com>
2022-06-19 15:16:39 -0300

    retirando botões de alerta e setas

--- marchaLentaNano/marchaLentaNano.ino
+++ marchaLentaNano/marchaLentaNano.ino
@@ -1,36 +1,37 @@
 #include <Servo.h>
-#include <LedControl.h>
 
+// Objeto da classe Servo.h
+Servo myServo;
+
+//pino e Attach servo motor
+int pinServo = 7;
+
+// pino madhca down e up
 int down = 2;
 int up = 3;
-int alert = 4;
-int right = 5;
-int left = 6;
-int pinServo = 7;
+
+//graus que de movimentação do servo motor
+int march_one = 33;
+int march_two = 50;
+int march_three = 65;
+int march_four = 80;
+int march_five = 95;
+int march_six = 107;
+int march_seven = 125;
+int march_eight = 155;
+
+// quantidade de marhcas
 int march = 8;
-
-const int DIN_PIN = 11;
-const int CS_PIN = 10;
-const int CLK_PIN = 13;
-
-Servo myServo;
-LedControl display = LedControl(DIN_PIN, CLK_PIN, CS_PIN);
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(down, OUTPUT);
   pinMode(up, OUTPUT);
-  pinMode(alert, OUTPUT);
-  pinMode(right, OUTPUT);
-  pinMode(left, OUTPUT);
 
   myServo.attach(pinServo);
-  myServo.write(155);
+  myServo.write(march_eight);
 
-  display.clearDisplay(0);
-  display.shutdown(0, false);
-  display.setIntensity(0, 15);
 }
 
 void loop()
@@ -45,21 +46,6 @@ void loop()
     marchDOWN();
   };
 
-  if (digitalRead(alert) == HIGH)
-  {
-    attention();
-  };
-
-  if (digitalRead(right) == HIGH)
-  {
-    rightArrow();
-  };
-
-  if (digitalRead(left) == HIGH)
-  {
-    leftArrow();
-  }
-
   Serial.print("March: ");
   Serial.println(march);
 }
@@ -69,37 +55,37 @@ void marchUP()
   march -= 1;
   if (digitalRead(up) == HIGH && march == 1)
   {
-    myServo.write(45);
+    myServo.write(march_one);
   };
 
   if (digitalRead(up) == HIGH && march == 2)
   {
-    myServo.write(61);
+    myServo.write(march_two);
   };
 
   if (digitalRead(up) == HIGH && march == 3)
   {
-    myServo.write(75);
+    myServo.write(march_three);
   };
 
   if (digitalRead(up) == HIGH && march == 4)
   {
-    myServo.write(88);
+    myServo.write(march_four);
   };
 
   if (digitalRead(up) == HIGH && march == 5)
   {
-    myServo.write(103);
+    myServo.write(march_five);
   };
 
   if (digitalRead(up) == HIGH && march == 6)
   {
-    myServo.write(117);
+    myServo.write(march_six);
   };
 
   if (digitalRead(up) == HIGH && march == 7)
   {
-    myServo.write(135);
+    myServo.write(march_seven);
   };
 
   if (march <= 1)
@@ -115,42 +101,42 @@ void marchDOWN()
   march += 1;
   if (digitalRead(down) == HIGH && march == 1)
   {
-    myServo.write(45);
+    myServo.write(march_seven);
   };
 
   if (digitalRead(down) == HIGH && march == 2)
   {
-    myServo.write(61);
+    myServo.write(march_two);
   };
 
   if (digitalRead(down) == HIGH && march == 3)
   {
-    myServo.write(75);
+    myServo.write(march_three);
   };
 
   if (digitalRead(down) == HIGH && march == 4)
   {
-    myServo.write(88);
+    myServo.write(march_four);
   };
 
   if (digitalRead(down) == HIGH && march == 5)
   {
-    myServo.write(103);
+    myServo.write(march_five);
   };
 
   if (digitalRead(down) == HIGH && march == 6)
   {
-    myServo.write(117);
+    myServo.write(march_six);
   };
 
   if (digitalRead(down) == HIGH && march == 7)
   {
-    myServo.write(135);
+    myServo.write(march_seven);
   };
 
   if (digitalRead(down) == HIGH && march == 8)
   {
-    myServo.write(155);
+    myServo.write(march_eight);
   };
 
   if (march >= 8)
@@ -158,73 +144,4 @@ void marchDOWN()
     march = 8;
   }
   delay(300);
-}
-
-void attention(){
-   int lenLedsAlert = 0;
-
-  const uint64_t ALERT[] = {
-  0xffffffffffffffff,
-  0x0000000000000000
-};
-  const int IMAGES_LEN = sizeof(ALERT)/8;
- 
-  while (digitalRead(alert) == HIGH)
-  {
-    displayImage(ALERT[lenLedsAlert]);
-
-    if (++lenLedsAlert >= IMAGES_LEN ) {
-      lenLedsAlert = 0;
-    }
-    delay(100);
-  }
-  display.clearDisplay(0);
-  
-};
-
-void rightArrow(){
-  int lenLedsRigh = 0;
-  const uint64_t  RIGHT[] = {
-  0x3c3c3c3cff7e3c18,
-  0x0000000000000000
-};
-const int IMAGES_LEN = sizeof(RIGHT)/8;
-  while (digitalRead(right) == HIGH)
-  {
-     displayImage(RIGHT[lenLedsRigh]);
-    if (++lenLedsRigh >= IMAGES_LEN ) {
-      lenLedsRigh = 0;
-    }
-    delay(100);
-  }
-  display.clearDisplay(0);
-};
-
-void leftArrow(){
-  int lenLedsLeft = 0;
-
-  const uint64_t LEFT[] = {
-    0x183c7eff3c3c3c3c,
-    0x0000000000000000
-    
-  };
-  const int IMAGES_LEN = sizeof(LEFT)/8;
-  while (digitalRead(left) == HIGH)
-  {
-     displayImage(LEFT[lenLedsLeft]);
-    if (++lenLedsLeft >= IMAGES_LEN ) {
-      lenLedsLeft = 0;
-    }
-    delay(100);
-  }
-  display.clearDisplay(0);
-};
-
-void displayImage(uint64_t image) {
-  for (int i = 0; i < 8; i++) {
-    byte row = (image >> i * 8) & 0xFF;
-    for (int j = 0; j < 8; j++) {
-      display.setLed(0, i, j, bitRead(row, j));
-    }
-  }
 }
